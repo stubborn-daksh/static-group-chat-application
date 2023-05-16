7bdfbfaf1fa41e3c1eaf1223c2b5ed77ee06a4fb
= <=>
2023-05-16 23:44:07 +0530

    socket-ready-

---
+++ app.js
@@ -0,0 +1,29 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var path = require('path');
+var io = require('socket.io')(http);
+app.get('/', function (req, res) {
+    var options = {
+        root: path.join(__dirname)
+    }
+    var fileName = 'index.html';
+    res.sendFile(fileName, options)
+})
+
+users = [];
+io.on('connection', function (socket) {
+    socket.on('setUserName', function (data) {
+        console.log(data + ' user connected')
+        if (users.indexOf(data) > -1) {
+            socket.emit('userExists', data + 'User name is already in use!, Please use another name')
+        } else {
+            users.push(data);
+            socket.emit('setUser', { username: data })
+        }
+    })
+})
+
+
+http.listen(3000, function () {
+    console.log('Server listening on 3000')
+})
